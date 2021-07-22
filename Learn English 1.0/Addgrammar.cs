7a55b80379b0611493ecca2167caef70f9305bc2
Albert Attila <albattis@freemail.hu>
2021-07-22 10:43:51 +0200

    add grammatic rule

---
+++ Learn English 1.0/Addgrammar.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Learn_English_1._0
+{
+    public partial class Addgrammar : Form
+    {
+        public Addgrammar()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var content=new learnenglishEntities())
+            {
+                int count=0;
+                foreach (var item in content.grammar)
+                {
+                     count = content.grammar.Count();
+                }
+
+                grammar gram = new grammar
+                {
+                    grammary_id = count,
+                    grammatical_rule = textBox2.Text,
+                    rule_name = textBox1.Text
+                };
+
+                content.grammar.Add(gram);
+                content.SaveChanges();
+
+
+            }
+        }
+    }
+}
--- Learn English 1.0/LearnEnglishDashBoard.Designer.cs
+++ Learn English 1.0/LearnEnglishDashBoard.Designer.cs
@@ -34,6 +34,7 @@ namespace Learn_English_1._0
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -86,12 +87,22 @@ namespace Learn_English_1._0
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(220, 213);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(183, 31);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Nyelvtani szabály felvétele";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
             // LearnEnglishDashBoard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.MediumSeaGreen;
             this.ClientSize = new System.Drawing.Size(640, 296);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -111,6 +122,7 @@ namespace Learn_English_1._0
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
